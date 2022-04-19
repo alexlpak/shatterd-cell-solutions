816d341323788a7c320b6c3e83a096cefd44c0e6
Alex Pak <42758999+alexlpak@users.noreply.github.com>
2022-04-18 21:40:40 -0500

    adding height to Calendly widget to prevent scrolling on mobile

--- src/components/CalendlyWidget.jsx
+++ src/components/CalendlyWidget.jsx
@@ -1,21 +1,16 @@
-import { InlineWidget } from 'react-calendly';
-import { useTheme } from 'styled-components';
+import styled from 'styled-components';
+
+const CalendlyStyled = styled.iframe.attrs({
+    src: 'https://calendly.com/alexlpak/30min?primary_color=0085DC&embed_type=Inline&embed_domain=1',
+    title: 'Calendly widget - schedule an appointment'
+})`
+    height: 120vh;
+    width: 100%;
+    overflow: hidden;
+`;
 
 const CalendlyWidget = () => {
-    const theme = useTheme();
-
-    return (
-        <InlineWidget
-            title='Calendly widget - schedule an appointment'
-            styles={{
-                height: '100vh'
-            }}
-            pageSettings={{
-                primaryColor: theme.colors.primary.main.replace('#', '')
-            }}
-            url='https://calendly.com/alexlpak/30min'
-        />
-    );
+    return <CalendlyStyled />;
 };
 
 export default CalendlyWidget;
