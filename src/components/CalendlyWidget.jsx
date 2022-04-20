3cc82237fc0ff412aa56c140922f17a61e545e3c
Alex Pak <42758999+alexlpak@users.noreply.github.com>
2022-04-20 16:06:13 -0500

    updated Calendly link to store owner's link

--- src/components/CalendlyWidget.jsx
+++ src/components/CalendlyWidget.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const CalendlyStyled = styled.iframe.attrs({
-    src: 'https://calendly.com/alexlpak/30min?primary_color=0085DC&embed_type=Inline&embed_domain=1',
+    src: 'https://calendly.com/shatterdcell?primary_color=0085DC&embed_type=Inline&embed_domain=1',
     title: 'Calendly widget - schedule an appointment'
 })`
     height: 120vh;
